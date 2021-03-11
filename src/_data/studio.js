210faae4d4b5d83827521e3766c71ed96c969225
Louis Maher <louismaher@gmail.com>
2021-03-11 12:20:42 -0500

    Studio en images

--- src/_data/studio.js
+++ src/_data/studio.js
@@ -3,14 +3,13 @@ const Cache = require('@11ty/eleventy-cache-assets');
 /**
  * Grabs the remote data for studio images and returns back
  * an array of objects
- *npm start
- 
+ *
  * @returns {Array} Empty or array of objects
  */
 module.exports = async () => {
   try {
     // Grabs either the fresh remote data or cached data (will always be fresh live)
-    const {items} = await Cache(
+    const {resources} = await Cache(
       'https://res.cloudinary.com/wikilouis/image/list/studio.json',
       {
         duration: '1d', // 1 day
@@ -18,7 +17,7 @@ module.exports = async () => {
       }
     );
 
-    return items;
+    return resources;
   } catch (ex) {
     console.log(ex);
 
